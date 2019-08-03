068fcad30adcce59845d513df43640d110640d97
Adrian Ostapkiewicz <ostapkiewicz.adrian@gmail.com>
2019-08-03 11:48:24 +0100

    started some work on component

--- js/script.js
+++ js/script.js
@@ -1,61 +1,56 @@
-import { readdirSync } from "fs";
 
-    var movies = [
-        {   id: 1,
-            title: 'Harry Potter',
-            desc: 'film o czarodzieju',
-            image: 'images/potter.jpg'
-        },
-        {   id: 2,
-            title: 'Matrix',
-            desc: 'film o symulacji',
-            image: 'images/matrix.jpg'
-        },
-        {   id: 3,
-            title: 'Snow piercer',
-            desc: 'film o ukladzie zycia',
-            image: 'images/snow.jpg'
-        },
-        {   id: 4,
-            title: 'Mortal Kombat',
-            desc: 'film o ratowaniu swiata',
-            image: 'images/mortal.jpg'
-        },
-        {   
-            id: 5,
-            title: 'Pulp Fiction',
-            desc: 'klasyk',
-            image: 'images/pulp.jpg'
-        }
-    ];
 
-    var moviesElements = movies.map(function(movie){
-        return React.createElement('li', {key: movie.id},
-            React.createElement('h2', {}, movie.title),
-            React.createElement('p', {}, movie.desc),
-            React.createElement('img', {src: movie.image, style:{width:300, height:300}})
-            );
-    });
 
-    var element = 
-        React.createElement('div', {},
-            React.createElement('h1', {}, 'Lista filmow'),
-            React.createElement('ul', {}, moviesElements)
-            );
+var movies = [
+    {   id: 1,
+        title: 'Harry Potter',
+        desc: 'film o czarodzieju',
+        image: 'images/potter.jpg'
+    },
+    {   id: 2,
+        title: 'Matrix',
+        desc: 'film o symulacji',
+        image: 'images/matrix.jpg'
+    },
+    {   id: 3,
+        title: 'Snow piercer',
+        desc: 'film o ukladzie zycia',
+        image: 'images/snow.jpg'
+    },
+    {   id: 4,
+        title: 'Mortal Kombat',
+        desc: 'film o ratowaniu swiata',
+        image: 'images/mortal.jpg'
+    },
+    {   
+        id: 5,
+        title: 'Pulp Fiction',
+        desc: 'klasyk',
+        image: 'images/pulp.jpg'
+    }
+];
 
-ReactDOM.render(element, document.getElementById('app'));
 
+// var movies = {
+//     id: 1,
+//     title: 'Harry Potter',
+//     desc: 'film o czarodzieju',
+//     image: 'images/potter.jpg'
+// };
 
 var Movie = React.createClass({
     propTypes: {
-        image: React.PropTypes.object.isRequired,
+        movies: React.PropTypes.object.isRequired,
     },
-    render: function() {
+    render: function () {
         return (
-            React.createElement('div', {},
-            React.createElement('h2', {}, this.props.image.name),
-            React.createElement('img', {src: this.props.image.src})
+            React.createElement('li', {key: movies.id},
+                React.createElement('img', {src: this.props.movies.image})
             )
         )
     },
 });
+
+
+var element = React.createElement(Movie, {key: movies.id, movies: movies});
+ReactDOM.render(element, document.getElementById('app'));
