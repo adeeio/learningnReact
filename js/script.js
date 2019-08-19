79e18dc9b9011b5b49d6a37a24ca5b647e612615
Adrian Ostapkiewicz <ostapkiewicz.adrian@gmail.com>
2019-08-19 21:03:40 +0100

    latest update

--- js/script.js
+++ js/script.js
@@ -43,8 +43,11 @@ var Movie = React.createClass({
     },
     render: function () {
         return (
-            React.createElement('ul', {},
-                React.createElement('li', { key: this.props.movie.id })
+                React.createElement('li', { key: this.props.movie.id},
+                React.createElement(MovieTitle, {title: this.props.movie.title}),
+                React.createElement(MovieImage, {image: this.props.movie.image, style: {widht: 300, hegith: 300}}),
+                React.createElement(MovieDescription, {desc: this.props.movie.desc})
+                
             )
         )
 
@@ -65,7 +68,7 @@ var MovieImage = React.createClass({
         image: React.PropTypes.object.isRequired,
     },
     render: function () {
-        return React.createElement('img', {src: this.props.image.image})
+        return React.createElement('img', {src: this.props.image})
     },
 
 });
@@ -79,7 +82,17 @@ var MovieTitle = React.createClass({
 
 });
 
+var MoviesList = React.createClass({
+    propTypes: {
+        moviesList: React.PropTypes.array.isRequired,
+    },
+    render: function () {
+        return moviesList.map(function (movie) {
+            return React.createElement(Movie, {movie: movie})
+        })
+    },
+});
 
-var element = React.createElement(Movie, { movie: movie[0]}); 
-var element2 = React.createElement(MovieTitle, {title: movie[0]})
+var element = React.createElement('div', {},
+React.createElement('ul',{},MoviesList)); 
 ReactDOM.render(element, document.getElementById('app'));
